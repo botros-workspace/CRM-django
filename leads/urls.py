7fe9e13cabff96509fe4f2e0cbae902a3e8dede3
David Botros <a01052944@unet.univie.ac.at>
2021-03-03 05:36:28 +0100

    + lead delete view

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import lead_list, lead_detail, lead_create, lead_update
+from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete
 
 app_name = "leads"
 
@@ -9,5 +9,6 @@ urlpatterns = [
     #we write other String types URLS
     path('<int:pk>/', lead_detail),
     path('<int:pk>/update/', lead_update),
+    path('<int:pk>/delete/', lead_delete),
     path('create/', lead_create),
 ]
--- leads/views.py
+++ leads/views.py
@@ -41,3 +41,8 @@ def lead_update(request,pk):
         'lead' : lead
     }
     return render(request, "lead-update.html", context = context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id= pk)
+    lead.delete()
+    return redirect("/leads")
