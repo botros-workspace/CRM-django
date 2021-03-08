6e733a4234b7bae7e29e902ba75ee06b94c2b108
David Botros <a01052944@unet.univie.ac.at>
2021-03-08 10:10:35 +0100

    + create a new user profile using signals

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     #U need to add the user Auth to the setting in the end (NOTE) 
@@ -29,3 +30,8 @@ class Agent(models.Model):
     def __str__(self):
         return self.user.email
     
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user = instance)
+
+post_save.connect(post_user_created_signal, sender= User)
