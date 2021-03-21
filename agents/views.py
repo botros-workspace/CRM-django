0e630a561d0b8b1177216f8a845538ad668c8111
David Botros <a01052944@unet.univie.ac.at>
2021-03-21 17:53:41 +0100

    + password reset logic

--- agents/views.py
+++ agents/views.py
@@ -1,3 +1,5 @@
+import random
+from django.core.mail import send_mail
 from django.shortcuts import render, reverse
 from django.views import generic
 from .mixins import OrganizerAndLoginRequiredMixin
@@ -8,8 +10,8 @@ class AgentListView(OrganizerAndLoginRequiredMixin, generic.ListView):
     template_name = "agent-list.html"
 
     def get_queryset(self):
-        organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation = organisation)
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization =  organization)     
 
 class AgentCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
     template_name = "agent-create.html"
@@ -19,9 +21,21 @@ class AgentCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit = False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit = False)
+        user.is_agent = True
+        user.is_organizer = False
+        user.set_password(f"{random.randint(0,10000000)}")
+        user.save()
+        Agent.objects.create(
+            user= user,
+            organization= self.request.user.userprofile
+        )
+        send_mail(
+            subject="Invetaion",
+            message="Yo were added as an agent on CRM. Please come login to start working",
+            from_email="admin@CRM.com",
+            recipient_list=[user.email]
+        )
 
         return super(AgentCreateView,self).form_valid(form)
 
@@ -29,8 +43,8 @@ class AgentDetailView(OrganizerAndLoginRequiredMixin, generic.DetailView):
     template_name = "agent-detail.html"
     
     def get_queryset(self):
-        organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation = organisation)
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization = organization)
 
 class AgentUpdateView(OrganizerAndLoginRequiredMixin, generic.UpdateView):
     template_name= "agent-update.html"
@@ -47,5 +61,5 @@ class AgentDeleteView(OrganizerAndLoginRequiredMixin, generic.DeleteView):
         return reverse("agents:agent-list")
 
     def get_queryset(self):
-        organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation = organisation)
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization = organization)
--- crm_django/urls.py
+++ crm_django/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import LandingPageView, SignupView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,7 +12,11 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name= "landing-page"),
     path('login/', LoginView.as_view(), name ="login"),
     path('logout/', LogoutView.as_view(), name ="logout"),
-    path('signup/', SignupView.as_view(), name ="signup")
+    path('signup/', SignupView.as_view(), name ="signup"),
+    path('reset-password/', PasswordResetView.as_view(), name ="reset-password"),
+    path('password_reset_done/', PasswordResetDoneView.as_view(), name ="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name ="password_reset_confirm"),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(), name ="password_reset_complete"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
